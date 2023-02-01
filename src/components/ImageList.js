a1cc68c4818cf81657be5977e317344670a25103
HajiraRafiqa <hajira.rafiqa@capgemini.com>
2023-02-01 15:34:53 +0530

    code added

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -3,6 +3,14 @@ import ImageShow from "./ImageShow";
 
 function ImageList({images})
 {
+  const readeredImages = images.map((image) => 
+  {
+    return(
+      <ImageShow /> 
+    )
+  });
+
+
   return(
     <div>
       ImageList: {images.length}
