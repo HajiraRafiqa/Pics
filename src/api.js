02eb33422a405556e0aba0d9f04737a18ffd6e83
HajiraRafiqa <hajira.rafiqa@capgemini.com>
2023-02-02 15:09:51 +0530

    api changes

--- src/api.js
+++ src/api.js
@@ -2,17 +2,8 @@ import axios from "axios";
 
 const searchImages = async (term) =>
 {
-    const response = await axios.get("https://api.unsplash.com/search/photos" ,
-    {
-        headers: 
-        {
-            Authorization: "Client-ID TgRChJqO2OC5OTmphnzeVS6hBW9MOzrcg2hNi1s9r9k"
-        },
-        params:
-        {
-            query: term,
-        },
-    });
+    const response = await axios.get("https://api.unsplash.com//search/photos?query=${term}&client_id=TgRChJqO2OC5OTmphnzeVS6hBW9MOzrcg2hNi1s9r9k" ,
+    );
 
     console.log(response);
     return response;
