37803cbe62b9567a19a5d615e6d6d2b9ecfbdc4b
HajiraRafiqa <hajira.rafiqa@capgemini.com>
2023-02-03 12:19:12 +0530

    changing the code for api

--- src/api.js
+++ src/api.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 const searchImages = async (term) =>
 {
-    const response = await axios.get("https://api.unsplash.com//search/photos?query=${term}&client_id=TgRChJqO2OC5OTmphnzeVS6hBW9MOzrcg2hNi1s9r9k" ,
+    console.log("This is term");
+    console.log(term);
+    const response = await axios.get(`https://api.unsplash.com//search/photos?query=${term}&client_id=TgRChJqO2OC5OTmphnzeVS6hBW9MOzrcg2hNi1s9r9k` ,
     );
 
     console.log(response);
