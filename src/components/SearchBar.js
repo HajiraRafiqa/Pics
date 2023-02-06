12488a419f8283f90f7bc57088ce080e82996268
HajiraRafiqa <hajira.rafiqa@capgemini.com>
2023-02-06 16:07:13 +0530

    code added

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-
+import "./SearchBar.css";
 
 function SearchBar({onSubmit})
 {
@@ -28,8 +28,9 @@ function SearchBar({onSubmit})
 
 
   return(
-    <div>
+    <div className="search-bar">
       <form onSubmit={handleFormSubmit}>
+        <label>Search</label>
         <input value={term} onChange={handleChange}/>
       </form>
     </div>
